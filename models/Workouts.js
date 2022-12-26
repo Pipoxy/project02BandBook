6f2ca5a43b24cd3e478787051c6f95c69a164b45
Jett <jettc505@gmail.com>
2022-12-26 12:30:43 -0700

    Conected the dattabase to the main code

--- models/Workouts.js
+++ models/Workouts.js
@@ -1,21 +1,27 @@
-const { Model, DataTypes } = require("sequelize");
+const { Model,DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-const User = require("./User")
 
-class Workouts extends Model {}
+class Workouts extends Model { }
 
 Workouts.init({
   user_id: {
     type: DataTypes.INTEGER,
     references: {
-      Model : "user",
+      Model: "user",
       key: "id"
     }
   },
   status: {
     type: DataTypes.BOOLEAN,
     defaultValue: false,
-  },
-});
+  }
+},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: "workouts"
+  }
+);
 
-module.exports = Workouts
+module.exports = Workouts;
