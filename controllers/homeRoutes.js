8cd13403d66b349035c105892a8a1ee49c2dfff3
Daniel Graves <68198944+danielcgraves@users.noreply.github.com>
2023-01-03 21:23:37 -0700

    Added the test-css code into the handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,46 +1,38 @@
-const router = require("express").Router();
-const { Workout, User } = require("../models");
+const router = require('express').Router();
+const { Workout, User } = require('../models');
 // const authorized = require("../helpers/authorized");
 
 // get all posts for homepage
 router.get('/', async (req, res) => {
-  try {
-
-
-    res.render('home');
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		res.render('login');
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/profile', async (req, res) => {
-  try {
-
-
-    res.render('profile');
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		res.render('profile');
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/login', async (req, res) => {
-  try {
-
-
-    res.render('login');
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		res.render('login');
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/signup', async (req, res) => {
-  try {
-
-
-    res.render('signup');
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		res.render('signup');
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
