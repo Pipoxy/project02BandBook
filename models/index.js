e9f6568116654e7275ff1f46f5e7634bc8ede7e3
Daniel Graves <68198944+danielcgraves@users.noreply.github.com>
2023-01-08 21:29:13 -0700

    trying to get the user input posting

--- models/index.js
+++ models/index.js
@@ -1,23 +1,13 @@
-const User = require("./User");
-const Workouts = require("./Workouts");
-const Exercises = require("./Exercises");
+const User = require('./User');
+const Workouts = require('./Workouts');
 
 User.hasMany(Workouts, {
-  foreignKey: "user_id",
-  onDelete: "CASCADE",
+	foreignKey: 'user_id',
+	onDelete: 'CASCADE',
 });
 
 Workouts.belongsTo(User, {
-  foreignKey: "user_id",
+	foreignKey: 'user_id',
 });
 
-Workouts.hasMany(Exercises, {
-  foreignKey: "workout_id",
-  onDelete: "CASCADE",
-});
-
-Exercises.belongsTo(Workouts, {
-  foreignKey: "workout_id",
-});
-
-module.exports = { User, Workouts, Exercises };
+module.exports = { User, Workouts };
