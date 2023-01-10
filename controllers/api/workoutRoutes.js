d98284f7b8791b463c4d4aeaee94961004a89924
Pipoxy <jparke5@hotmail.com>
2023-01-09 20:50:02 -0700

    added the finishing touches

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,37 +1,50 @@
-const router = require('express').Router();
-const { Workouts } = require('../../models');
-const authorized = require('../../utils/auth');
+const router = require("express").Router();
+const { Workouts } = require("../../models");
+const authorized = require("../../utils/auth");
 
-router.post('/', async (req, res) => {
-	try {
-		const newWorkout = await Workouts.create({
-			...req.body,
-			user_id: req.session.user_id,
-		});
-		res.status(200).json(newWorkout);
-	} catch (err) {
-		res.status(400).json(err);
-	}
+router.post("/", async (req, res) => {
+  try {
+    const newWorkout = await Workouts.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newWorkout);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', authorized, async (req, res) => {
-	try {
-		const workoutData = await Workouts.destroy({
-			where: {
-				id: req.params.id,
-				user_id: req.session.user_id,
-			},
-		});
+router.get("/", async (req, res) => {
+  try {
+    const workoutData = await Workouts.findAll({
+      limit: 3,
+      order: [["id", "DESC"]],
+    });
 
-		if (!workoutData) {
-			res.status(404).json({ message: 'No workout found!' });
-			return;
-		}
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-		res.status(200).json(workoutData);
-	} catch (err) {
-		res.status(500).json(err);
-	}
+router.delete("/:id", authorized, async (req, res) => {
+  try {
+    const workoutData = await Workouts.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!workoutData) {
+      res.status(404).json({ message: "No workout found!" });
+      return;
+    }
+
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
